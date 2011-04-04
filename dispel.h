cb38eeee0996b5029beee647477d178649267fe5
James Churchill <james@rfdesign.com.au>
2011-04-05 03:02:46 +1000

    Initial commit.

---
+++ dispel.h
@@ -0,0 +1,7 @@
+/* dispel.h
+ * Header file for DisPel
+ * James Churchill
+ * Created 240900
+ */
+
+int disasm(unsigned char *mem, unsigned long pos, unsigned char *flag, char *inst, unsigned char tsrc);
